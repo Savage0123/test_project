ea0f5d04b9c8b6dd9788b0f1fd64648e81e7a7e5
savage boy <trainordie21@gmail.com>
2024-06-19 00:42:39 +0300

    homework 10_1 done

--- test_poetry/src/processing.py
+++ test_poetry/src/processing.py
@@ -1,12 +1,12 @@
 def filter_by_state(dictionary_list: list, state: str = "EXECUTED") -> list:
     """Функция принимает на вход список словарей и значение для ключа state и возвращает новый список, содержащий
     словари, у которых ключ state содержит переданное в функцию значение"""
-    new_list_1 = []
+    result_list = []
 
-    for i in dictionary_list:
-        if i.get("state") == state:
-            new_list_1.append(i)
-    return new_list_1
+    for item in dictionary_list:
+        if item.get("state") == state:
+            result_list.append(item)
+    return result_list
 
 
 print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -14,7 +14,7 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
-def sort_by_date(date_list: list, ascending = False) -> list:
+def sort_by_date(date_list: list, ascending = True) -> list:
     """Функция принимает на вход список словарей и возвращает новый список с отсортированными по убыванию даты словари"""
     sorted_list = sorted(date_list, key = lambda x: x["date"], reverse = ascending)
     return sorted_list
