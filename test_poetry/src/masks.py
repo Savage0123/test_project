6861798a90e6c963d5506f973d1a10b7de22c6e1
savage boy <trainordie21@gmail.com>
2024-09-08 23:15:05 +0300

    homework 12_2 done

--- test_poetry/src/masks.py
+++ test_poetry/src/masks.py
@@ -1,12 +1,21 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(f"../logs/src.masks.log", mode="w")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
+    logger.info("Маскируем номер карты")
     return f"{card[0:7]}** **** {card[-4:]}"
 
 
 def get_mask_account(account: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
+    logger.info("Маскируем номер счета")
     return "*" * len(account[13:-4]) + account[-4:]
-
-
-# print(get_mask_card_number("2222 3333 4444 5555"))
-# print(get_mask_account("2222 3333 4444 5555"))
