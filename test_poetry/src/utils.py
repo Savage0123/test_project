99e6e2d5c86f276de526a5b75d7d765e824231cd
savage boy <trainordie21@gmail.com>
2024-11-12 00:26:40 +0300

    Homework 12_2/13_1

--- test_poetry/src/utils.py
+++ test_poetry/src/utils.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler(f"../logs/{__name__}.log", mode="w")
+file_handler = logging.FileHandler(f"../logs/src.masks.log", mode="w")
 file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
